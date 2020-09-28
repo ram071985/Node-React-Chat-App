dc9a764817e7d132b8e2b2933ad0596cac64980b
ram071985 <reidmuchow@gmail.com>
2020-09-28 15:16:57 -0500

    added more css and bootstrap in UI for ChatRoom.

--- client/src/ChatRoom.js
+++ client/src/ChatRoom.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
-
+import { User } from "react-feather";
 
 class ChatRoom extends Component {
-    constructor() {
-        super();
-        this.state = {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-        };
-    }
-
-    render() {
-
-        return (
-            <div className="container-fluid justify-content-center main-container">
-                <div className="container-fluid justify-content-center inner-container">
-            yowzers this isn't working
-                </div>
+  render() {
+    return (
+      <div className="container-fluid justify-content-center main-container">
+        <div className="container-fluid outline-container">
+          <div className="container-fluid inner-container">
+            <div className="row user-row">
+              <div className="col-6 user-column">
+                <h6 className="text-left inline-block username-text">
+                  <User
+                    width="25"
+                    color="rgb(177, 149, 127)"
+                    className="mr-2 mb-2 user-icon"
+                  />{" "}
+                  Reid Muchow
+                </h6>
+              </div>
             </div>
-        );
-    }
+            <div className="container-fluid messages-container"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ChatRoom;
