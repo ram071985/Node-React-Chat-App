1a41f69a6b846e1f22e089d846de6bb58d2832c5
ram071985 <reidmuchow@gmail.com>
2020-11-30 19:25:22 -0600

    added create user query to register endpoint.

--- server.js
+++ server.js
@@ -7,19 +7,41 @@ const http = require("http");
 const server = http.createServer(http);
 const socketio = require("socket.io");
 const io = socketio(server);
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const exjwt = require("express-jwt");
+
 require("dotenv").config();
 
 app.use("/", express.static(path.join(__dirname, "client/build")));
 
+const jwtMW = exjwt({
+  secret: process.env.SECRET
+})
+
 pgDataAccess.dbConnection();
 
+app.post("/api/register", async (req, res) => {
+  let pool = await pgDataAccess.dbConnection();
+  const { username, password } = req.body;
+  const hash = await bcrypt.hashSync(password, saltRounds);
+
+  await pool.query("BEGIN");
+  const result = await pool.query(`SELECT * FROM users WHERE username = "${username}";`
+  );
+  if (!result) {
+    res.status(401).json({message: "Username already exists"})
+  }
+
+
+
+});
+
 app.post("/api/authorize", async (req, res) => {
-  const config = {
-    params: {
-      username: req.query.username,
-      password: req.query.password,
-    },
-  };
+  const { username, password } = req.body;
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
