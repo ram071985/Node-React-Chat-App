339f2dffdc04c09ea808e035777171e7010ed15b
ram071985 <reidmuchow@gmail.com>
2020-11-30 19:39:34 -0600

    added try and catchfor INSERT.

--- server.js
+++ server.js
@@ -18,30 +18,41 @@ require("dotenv").config();
 app.use("/", express.static(path.join(__dirname, "client/build")));
 
 const jwtMW = exjwt({
-  secret: process.env.SECRET
-})
+  secret: process.env.SECRET,
+});
 
 pgDataAccess.dbConnection();
 
 app.post("/api/register", async (req, res) => {
   let pool = await pgDataAccess.dbConnection();
   const { username, password } = req.body;
+
   const hash = await bcrypt.hashSync(password, saltRounds);
 
   await pool.query("BEGIN");
-  const result = await pool.query(`SELECT * FROM users WHERE username = "${username}";`
+  const result = await pool.query(
+    `SELECT * FROM users WHERE username = "${username}";`
   );
   if (!result) {
-    res.status(401).json({message: "Username already exists"})
+    res.status(401).json({ message: "Username already exists" });
   }
 
+  try {
+  const insertResult = await pool.query(
+    "INSERT INTO users(username, password) VALUES($1, $2)",
+    [username, hash]
+  );
 
+  await pool.query("COMMIT");
+  } catch(err) {
+    
+  }
 
 });
 
+
 app.post("/api/authorize", async (req, res) => {
   const { username, password } = req.body;
-
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
