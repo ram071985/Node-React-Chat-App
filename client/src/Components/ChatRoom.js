96bc691076f4b22a3d883846202f48b967bd9af3
ram071985 <reidmuchow@gmail.com>
2020-12-06 13:09:44 -0600

    added text input form for typing messages to chatroom.

--- client/src/Components/ChatRoom.js
+++ client/src/Components/ChatRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { User } from "react-feather";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 class ChatRoom extends Component {
   constructor() {
@@ -20,12 +20,17 @@ class ChatRoom extends Component {
   render() {
     return (
       <div className="container-fluid chatroom-container">
-        <Row className="d-inline-block left-row">
+        <Row className="d-inline-block left-row no-gutters">
           <Col className="user-col"></Col>
         </Row>
-        <Row className="mt-5 d-inline-block right-row">
+        <Row className="mt-5 d-inline-block right-row no-gutters">
           <Col className="d-inline-block message-col">
             <div className="mt-3 chat-bubble">Howdy.</div>
+          </Col>
+          <Col className="type-col">
+            <Form.Group>
+              <Form.Control className="message-input" size="sm" type="input" placeholder="Small text" />
+            </Form.Group>
           </Col>
         </Row>
       </div>
