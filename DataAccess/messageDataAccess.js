5d5f7bf444d5c273a6ae3d52df37789f6da9958b
ram071985 <reidmuchow@gmail.com>
2020-12-06 14:57:45 -0600

    added messade data access and route.

---
+++ DataAccess/messageDataAccess.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const pgDataAccess = require("./pgDataAccess");
+
+createMessage = async (userId, text) => {
+  let pool = await pgDataAccess.dbConnection();
+
+  try {
+    await pool.query("BEGIN");
+    const result = await pool.query(
+      "INSERT INTO messages(user_id, text) VALUES($1, $2) RETURNING id, created_date, user_id, text"[
+        (userId, text)
+      ]
+    );
+    await pool.query("UPDATE users SET last_active_at = NOW() WHERE id = $1,", [
+      userId,
+    ]);
+
+    await pool.query("COMMIT");
+    return result.rows[0];
+  } catch (err) {
+    console.log(err);
+    await pool.query("ROLLBACK");
+  }
+};
+
+module.exports = { createMessage };
---
+++ Routes/messageRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const authDataAccess = require("../DataAccess/messageDataAccess");
+
+
+router.post("/messages", async (req,res) => {
+    let newMessage = {
+        id: req.body.id,
+        username = req.body.username,
+        text: req.body.text
+    }
+
+    const newMessageId = req.body.id;
+    const newMessageText = req.body.id;
+
+    let responseMessage = await messageDataAccess.createMessage(
+        newMessageId, newMessageText
+    )
+
+    returnedMessage.username = newMessage.username;
+})
+module.exports = router;
