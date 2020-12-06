c0d64c6819c4956884ad7d8b1f2a785907b4ea2c
ram071985 <reidmuchow@gmail.com>
2020-12-05 22:57:11 -0600

    changed css for rows and columns in chatroom.

--- client/src/Components/ChatRoom.js
+++ client/src/Components/ChatRoom.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { User } from "react-feather";
+import { Container, Row, Col } from "react-bootstrap";
 
 class ChatRoom extends Component {
   constructor() {
@@ -18,33 +19,17 @@ class ChatRoom extends Component {
 
   render() {
     return (
-      <div className="container-fluid justify-content-center main-container">
-        <div className="container-fluid outline-container">
-          <div className="container-fluid inner-container">
-            <div className="row user-row">
-              <div className="col-6 user-column">
-                <h6 className="text-left inline-block username-text">
-                  <User
-                    width="25"
-                    color="rgb(177, 149, 127)"
-                    className="mb-2 user-icon"
-                  />{" "}
-                  Reid Muchow
-                </h6>
-              </div>
-            </div>
-            <div className="container-fluid chat-container">            
-              <div className="container-fluid channel-name-container">
-                <h4 className="text-center channel-text">Main Channel</h4>
-              </div>
-              <div className="container-fluid messages-container">
-              </div>
-              <div className="container-fluid">
-              </div>              
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container className="container-fluid chatroom-container">
+        <Row className="h-100 mt-5 d-inline-block left-row">
+          <Col className="user-col">Hey</Col>
+        </Row>
+        <Row className="h-100 mt-5 d-inline-block right-row">
+          <Col className="d-inline-block message-col">Howdy</Col>
+        </Row>
+        <Row className="mt-1 type-row">
+          <Col className="d-inline-block type-col">Doo</Col>
+        </Row>
+      </Container>
     );
   }
 }
